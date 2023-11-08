6b5daaf363122cd803211f652c6adb1f6ad7e9e8
victor gil <baskeboler@gmail.com>
2023-11-08 14:47:51 -0300

    tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/icon.jpg",
       },
     },
     "gatsby-plugin-mdx",
@@ -34,6 +34,38 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-K7QLMN9B",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+  
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+        // Defaults to false
+        enableWebVitalsTracking: true,
+        // Defaults to https://www.googletagmanager.com
+        // selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
+      },
     },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,15 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+const Header = () => {
+  return ( 
+  <div>
+    <h1 class="sm:text-5xl text-3xl mb-4 font-medium bg-clip-text text-transparent bg-gradient-to-r from-emerald-200 to-blue-500">
+      Los 15 de Olivia
+    </h1>
+  </div> );
 }
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-
+ 
 
 
 
@@ -27,14 +18,14 @@ const linkStyle = {
 const IndexPage = ({data}) => {
   return (
     <main >
-       
      <section class="text-gray-600 body-font">
        <div class="container px-5 py-24 mx-auto flex flex-wrap">
          <div class="flex w-full mb-20 flex-wrap">
-           <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Los 15 de Olivia</h1>
+    <Header></Header>    
+           {/* <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Los 15 de Olivia</h1> */}
            <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Club de Golf, 2023</p>
          </div>
-         <div class="flex flex-wrap md:-m-2 -m-1">
+         <div class="flex flex-wrap md:-m-2 -m-1 rounded">
          {data.allFile.edges.map(e => (
           <GatsbyImage className="w-full md:w-1/4 lg:w-1/6" image={e.node.childImageSharp.thumb}/>
         ))}
@@ -53,13 +44,11 @@ query ImagesForGallery {
   allFile(filter: {relativePath: {regex: "/oli/"}}) {
     edges {
       node {
-        relativePath
         childImageSharp {
           thumb: gatsbyImageData(
             width: 320
             height: 320
-            layout: CONSTRAINED
-            placeholder: DOMINANT_COLOR
+            placeholder: BLURRED
             transformOptions: {cropFocus: NORTH}
           )
         }
@@ -71,4 +60,4 @@ query ImagesForGallery {
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Los 15 de Olivia</title>
