44f77584f764f4f827b7e58eef8228021aa23972
victor gil <baskeboler@gmail.com>
2023-11-12 21:52:58 -0300

    mobile viewing tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,13 +19,13 @@ const IndexPage = ({ data }) => {
   return (
     <main>
       <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+        <div className="container pt-10 sm:px-5 sm:py-24 mx-auto flex flex-wrap">
        
-          <div className="flex w-full mb-20 flex-wrap">
-            <h1 className="lg:w-2/3 sm:text-5xl text-3xl mb-4 font-medium bg-clip-text text-transparent bg-gradient-to-br from-pink-500 text to-pink-200 blue-300">
+          <div className="flex w-ful ml-5 sm:ml-0 mb-5 sm:mb-20 flex-wrap">
+            <h1 className="lg:w-1/3 sm:text-5xl text-3xl mb-4 font-medium bg-clip-text text-transparent bg-gradient-to-br from-pink-500 text to-pink-200 blue-300">
         Los 15 de Olivia
       </h1>
-            <p className="lg:pl-6 lg:w-1/3 mx-auto leading-relaxed flex flex-row text-base align-text-bottom">
+            <p className="pl-6 lg:w-2/3 mx-auto leading-relaxed flex flex-row text-base align-text-bottom">
               Club de Golf, 14 de Octubre 2023
             </p>
           </div>
